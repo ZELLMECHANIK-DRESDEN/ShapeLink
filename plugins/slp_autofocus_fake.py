a19cbd5f56238c255247c17ae30b7e4da51e0197
Eoghan O'Connell <eoclives@hotmail.com>
2021-06-15 13:39:50 +0200

    plugin: expand the fake autofocus plugin

--- plugins/slp_autofocus_fake.py
+++ plugins/slp_autofocus_fake.py
@@ -1,13 +1,18 @@
 
+import pathlib
+
 import cv2
 import nrefocus
 import qpimage
+import numpy as np
 
 from shapelink import ShapeLinkPlugin
 
 
 # open and crop hologram image
-img = cv2.imread("hologram_cell_curved_bg.jpg", cv2.IMREAD_GRAYSCALE)
+holo_im = pathlib.Path(__file__).parents[1] / \
+          "tests" / "data" / "hologram_cell_curved_bg.jpg"
+img = cv2.imread(str(holo_im), cv2.IMREAD_GRAYSCALE)
 img_crop = img[300:500, 100:300]
 
 
@@ -30,28 +35,30 @@ class AutofocusFakePlugin(ShapeLinkPlugin):
     def handle_event(self, event_data):
         """Handle a new event"""
 
-        # send image but use the loaded (cropped) hologram
-        # open with qpimage (or correct program)
-        # get phase
-        # use nrefocus to autofocus it
-        # get direction and magnitude of defocus needed
-        # how to pass this value to another program?
+        # retrieve the hologram image
+        # here is just an example, as our rtdc file does
+        # not have a hologram, so we use a fake dataset (see above)
+        should_be_the_hologram = event_data.images[0]
 
-        qpi = qpimage.QPImage(data=img_crop, which_data="hologram")
+        # create some fake noise
+        noise = np.random.rand(img_crop.shape[-1], img_crop.shape[-2])
+        # use qpimage to process the hologram
+        qpi = qpimage.QPImage(data=img_crop+noise,
+                              which_data="hologram")
         field = qpi.field
 
         # example nrefocus parameters
-        focus_range = (-20, 20)
+        focus_range = (30, 35)
         pixel_size = 0.34
         light_wavelength = 0.532
-
+        # use nrefocus to calculate autofocus
         afocus = nrefocus.RefocusNumpy(
             field, light_wavelength, pixel_size)
         focus_distance = afocus.autofocus(focus_range)
 
         # send to objective the absolute focus value
-        # this vaue must first be calibrated with the z-stage
-        #  via a log file? Write the focus values to log?
+        # this value must first be calibrated with the z-stage
+        # via a log file? Write the focus values to log?
         # emulate here with print
         print(focus_distance)
 
@@ -61,7 +68,7 @@ class AutofocusFakePlugin(ShapeLinkPlugin):
 info = {
     "class": AutofocusFakePlugin,
     "description": "Autofocus a fake hologram image using "
-                   "nrefocus and qpimage.",
+                   "nrefocus and qpimage",
     "name": "Autofocus",
     "version": "0.1.1",
 }
