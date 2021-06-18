a9c4e10ddb62f524b90de5a7f09a6777389f9942
Eoghan O'Connell <eoclives@hotmail.com>
2021-06-18 12:36:46 +0200

    test: add refocus test after propagate

--- tests/test_autofocus.py
+++ tests/test_autofocus.py
@@ -1,4 +1,5 @@
 import pathlib
+import tempfile
 import threading
 import numpy as np
 import cv2
@@ -71,6 +72,48 @@ def test_run_plugin_with_user_defined_trace_features():
     # start plugin
     for ii in range(49):
         p.handle_messages()
+
+
+def test_defocus_and_find_correct_focus():
+    # open jpg
+    holo_im = data_dir / "hologram_cell_curved_bg.jpg"
+    img = cv2.imread(str(holo_im), cv2.IMREAD_GRAYSCALE)
+    img_crop = img[300:500, 100:300]
+
+    # defocus image
+    qpi = qpimage.QPImage(data=img_crop,
+                          which_data="hologram")
+    field = qpi.field
+    focus_range = (0, 40e-6)
+    pixel_size = 0.34e-6
+    light_wavelength = 532e-9
+    initial_distance = 0.0
+    afocus = nrefocus.RefocusNumpy(
+        field, light_wavelength, pixel_size, distance=initial_distance)
+    focus1 = afocus.autofocus(focus_range)
+    assert np.isclose(focus1, 3.3086177092746745e-05)
+
+    # move it by 10e-6
+    prop_val = 10e-6
+    prop_field = afocus.propagate(prop_val)
+
+    # save in temp file
+    tpath = pathlib.Path(tempfile.mkdtemp())
+    expath = tpath / "exported.npy"
+    with expath as exp:
+        np.save(exp, prop_field)
+        # open defocused field
+        prop_loaded = np.load(str(expath))
+        qpi2 = qpimage.QPImage(data=prop_loaded, which_data="field")
+        field2 = qpi2.field
+        afocus2 = nrefocus.RefocusNumpy(
+            field2, light_wavelength, pixel_size, distance=prop_val)
+        # find focus of defocused image
+        focus2 = afocus2.autofocus(focus_range)
+        # focus2 is different to focus1 for some reason.
+        # Reported in github.com/RI-imaging/nrefocus/issues/13
+        assert np.isclose(focus2, 3.060066446129425e-05)
+        assert not np.isclose(focus1, focus2)
 
 
 if __name__ == "__main__":
